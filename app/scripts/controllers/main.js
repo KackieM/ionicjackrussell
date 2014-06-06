d48d15e60fa20d6a7dd41a0870a9bb8c31f85679
Katherine Mattison <kbmedisto@gmail.com>
2014-06-06 15:41:40 -0400

    do it

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('rwreceiptmgrApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
